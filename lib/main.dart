b4bbdea0e0f9e33fcf073049454f575637925959
Joshua Erinosho <joshuaerinosho@gmail.com>
2022-08-12 15:15:54 +0100

    Add internet download speed provider

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'theme_data.dart';
 import 'views/home_screen.dart';
 
-import 'package:flutter_speedtest/flutter_speedtest.dart';
-
 void main() {
   runApp(MyApp());
 }
@@ -27,59 +25,8 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  double downloadRate = 0;
-  double uploadRate = 0;
-
-  final _speedtest = FlutterSpeedtest(
-    baseUrl: 'http://speedtest.jaosing.com:8080', // your server url
-    pathDownload: '/download',
-    pathUpload: '/upload',
-    pathResponseTime: '/ping',
-  );
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Plugin example app'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text('Download: $downloadRate'),
-              Text('Upload: $uploadRate'),
-              Row(
-                children: [
-                  TextButton(
-                      onPressed: () async {
-                        _speedtest.getDataspeedtest(
-                          downloadOnProgress: ((percent, transferRate) {
-                            setState(() {
-                              downloadRate = percent;
-                            });
-                          }),
-                          uploadOnProgress: ((percent, transferRate) {
-                            setState(() {
-                              uploadRate = percent;
-                            });
-                          }),
-                          progressResponse: ((responseTime, jitter) {
-                            //TODO: in ui
-                          }),
-                          onError: ((errorMessage) {
-                            //TODO: in ui
-                          }),
-                        );
-                      },
-                      child: Text('Test '))
-                ],
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
+    return MaterialApp(home: HomeScreen());
   }
 }
--- lib/providers/download_speed_provider.dart
+++ lib/providers/download_speed_provider.dart
@@ -1,14 +1,17 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_speedtest/flutter_speedtest.dart';
 
 final downloadSpeedProvider =
-    StateNotifierProvider<DownloadSpeedNotifier, double>((ref) {
+    ChangeNotifierProvider<DownloadSpeedNotifier>((ref) {
   return DownloadSpeedNotifier();
 });
 
-class DownloadSpeedNotifier extends StateNotifier<double> {
-  DownloadSpeedNotifier() : super(0);
+final downloadSpeedUpdateProvider = StreamProvider<double>((ref) {
+  return ref.read(downloadSpeedProvider).downSpeedStream();
+});
 
+class DownloadSpeedNotifier extends ChangeNotifier {
   //Setup speedtest
   final _speedtest = FlutterSpeedtest(
     baseUrl: 'http://speedtest.jaosing.com:8080', // your server url
@@ -17,5 +20,18 @@ class DownloadSpeedNotifier extends StateNotifier<double> {
     pathResponseTime: '/ping',
   );
 
-  void increment() => state++;
+  Future<double> getDownSpeed() async {
+    double downloadRate = 0;
+    await _speedtest.getDataspeedtest(
+      downloadOnProgress: ((percent, transferRate) {
+        downloadRate = percent;
+      }),
+      uploadOnProgress: ((p, tr) {}),
+      progressResponse: ((r, j) {}),
+      onError: ((e) {}),
+    );
+    return downloadRate;
+  }
+
+  Stream<double> downSpeedStream() => Stream.fromFuture(getDownSpeed());
 }
